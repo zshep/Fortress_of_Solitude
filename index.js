81e66bd4e73a0def70f7e65c6a48caffef5bebd9
zsheperdigian <zsheperdigian@gmail.com>
2022-12-03 10:43:11 -0800

    first commit, started npm init and added .gitignore

---
+++ index.js
@@ -0,0 +1,8 @@
+// Random function just to test out functionality/ something to push to github FEEL FREE TO DELETE
+function ImArealApp () {
+
+        return console.log('Hello World');
+
+}
+
+ImArealApp();
