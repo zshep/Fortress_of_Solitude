0f1a4ab00dcc05747a5496bce5872a6473a4f142
tonytran97 <tonytran_97@live.com>
2022-12-06 18:14:59 -0800

    + starting typeDefs w/ query

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,19 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    PP: INT
+
+# Query    
+type Query {
+    users: [User]
+    user(username: String!): User
+    }
+}`
+
+
+module.exports = typeDefs;
