ee8637ff133f8b69285afb8c6a96facad2649678
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-12-07 11:04:54 -0800

    hashing added for password

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 const PostSchema = require ('./Post');
+var bcrypt = require('bcrypt');
+var saltFactor = 10;
 
 const UserSchema = new Schema({
     username: {
@@ -31,9 +33,28 @@ const UserSchema = new Schema({
         toJSON: {
             virtuals: true,
         },
-    
   });
 
+//CREATE A METHOD TO HASH USER PASSWORDS
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(saltFactor, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
   const User = model('User', UserSchema);
 
 module.exports = User;
