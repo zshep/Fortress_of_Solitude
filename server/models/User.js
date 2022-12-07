ff1eb90b1f1072d0fb11b67ea4d548eacb92583d
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-12-06 21:30:07 -0800

    user schema added

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    Post: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+  });
+
+  const User = model('User', UserSchema);
+
+module.exports = User;
