07a2de4533b46f65722fcc6fd80466b99f37b523
tonytran97 <tonytran_97@live.com>
2022-12-07 17:31:05 -0800

    refactor userSeeds to fit schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,60 +1,60 @@
 const { Schema, model } = require('mongoose');
-const PostSchema = require ('./Post');
+// const PostSchema = require('./Post');
 var bcrypt = require('bcrypt');
 var saltFactor = 10;
 
 const UserSchema = new Schema({
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5,
-    },
-    // Post: [
-    //   {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Post',
-    //   },
-    // ],
-    publishedPosts: [
-        PostSchema
-    ],
-        toJSON: {
-            virtuals: true,
-        },
-  });
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  // Post: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: 'Post',
+  //   },
+  // ],
+  // publishedPosts: [
+  //     PostSchema
+  // ],
+  //     toJSON: {
+  //         virtuals: true,
+  //     },
+});
 
 //CREATE A METHOD TO HASH USER PASSWORDS
-UserSchema.pre('save', function(next) {
-    var user = this;
+UserSchema.pre('save', function (next) {
+  var user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(saltFactor, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(saltFactor, function (err, salt) {
+    if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
     });
+  });
 });
-  const User = model('User', UserSchema);
+const User = model('User', UserSchema);
 
 module.exports = User;
