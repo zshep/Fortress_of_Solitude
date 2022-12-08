8ed5fe86e0614897fed6d9932ed6034aea5e01d6
tonytran97 <tonytran_97@live.com>
2022-12-07 17:20:21 -0800

    additional timestamp option to PostSchema

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,36 +2,37 @@ const { Schema, model } = require('mongoose');
 //const timestamp
 
 const PostSchema = new Schema({
-    PostTitle: {
-        type: String,
-        required: true,
-        maxlength: 69,
-        trim: true,
-    },
-    PostCategory: {
-        type: String,
-        required: true,
-        maxlength: 69,
-        trim: true,
-    },
-    PostText: {
-      type: String,
-      required: 'You need to write a decription!',
-      minlength: 1,
-      maxlength: 280,
-      trim: true,
-    },
-    PostUser: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
+  PostTitle: {
+    type: String,
+    required: true,
+    maxlength: 69,
+    trim: true,
+  },
+  PostCategory: {
+    type: String,
+    required: true,
+    maxlength: 69,
+    trim: true,
+  },
+  PostText: {
+    type: String,
+    required: 'You need to write a decription!',
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  PostUser: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
     //   add timestamp
-    },
-})
+  },
+  // timestamp
+}, { timestamps: true })
 
 const Post = model('Post', PostSchema);
 
