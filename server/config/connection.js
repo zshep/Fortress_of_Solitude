c7e40e32cfb159b3b12bcaaed1fc1e59c54015f9
tonytran97 <tonytran_97@live.com>
2022-12-07 17:13:43 -0800

    + connection to database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
