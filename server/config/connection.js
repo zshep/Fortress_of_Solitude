60f0a5cbbb36bab8f807c74e54656fdb320e0956
tonytran97 <tonytran_97@live.com>
2022-12-07 18:09:38 -0800

    go-mode db created, issues seeding dummy data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/go-mode', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
