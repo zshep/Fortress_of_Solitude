8a901cdfe67a46e318e64d7914a45432f68a9ba3
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-12-07 18:54:02 -0800

    building dashboard for rendering tasks

--- client/src/components/Card/ProfileCard.js
+++ client/src/components/Card/ProfileCard.js
@@ -1,21 +1,41 @@
 import React from "react";
-import CardContainer from './CardContainer'
-import CardHeader from './CardHeader'
-import CardContent from './CardContent'
-import CardFooter from './CardFooter'
-import PComponent from '../PComponent'
+import CardContainer from "./CardContainer";
+import CardHeader from "./CardHeader";
+import CardContent from "./CardContent";
+import CardFooter from "./CardFooter";
+import PComponent from "../PComponent";
+import Columnsss from "../Column/Columnsss";
+import Column from "../Column/Column";
 import ProfilePic from "../ProfilePic";
 
-function profileCard() {
-    return (
-        <CardContainer>
-            <CardHeader>
-                <PComponent>Users Dashboard</PComponent>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter></CardFooter>
-        </CardContainer>
-    )
+function ProfileCard() {
+  return (
+    <CardContainer>
+      <CardHeader>
+        <PComponent attr="card-header-title">Users Dashboard</PComponent>
+      </CardHeader>
+      <CardContent>
+        <Columnsss>
+          <Column attr="is-one-quarter">
+            <ProfilePic />
+          </Column>
+          <Column>
+            <> 
+            {/* Need empty element wrapping Pcomponents to prevent Column component from treating them as an array and generating new columns */}
+              <PComponent>Username</PComponent>
+              <PComponent>Community</PComponent>
+              <PComponent>PP level</PComponent>
+            </>
+          </Column>
+        </Columnsss>
+      </CardContent>
+      <CardFooter>
+        <a href="/board" className="card-footer-item">
+          See All Posts
+        </a>
+      </CardFooter>
+    </CardContainer>
+  );
 }
 
-export default profileCard
+export default ProfileCard;
--- client/src/components/ProfileBox.js
+++ client/src/components/ProfileBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfilePic from "./ProfilePic";
+import ProfileCard from "./Card/ProfileCard";
 
 function ProfileBox() {
   const data = {
@@ -33,44 +34,7 @@ function ProfileBox() {
     ]
   };
   return (
-    <div className="container">
-      <header className="card-header">
-        <p className="card-header-title">User's Profile Page</p>
-        <button className="card-header-icon" aria-label="more options">
-          <span className="icon">
-            <i className="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </button>
-      </header>
-      <div className="card-content">
-        <div className="columns">
-          <div className="column is-one-quarter">
-            <ProfilePic />
-          </div>
-          <div className="column">
-            <p> Username </p>
-            <p> Community Name</p>
-            <p> PP Level</p>
-          </div>
-        </div>
-        <div className="content">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </div>
-      </div>
-      <footer className="card-footer">
-        <a href="/board" className="card-footer-item">
-          See All Posts
-        </a>
-      
-        {/* We need to change this second one to render posts by user once back end is set up */}
-      </footer>
-    </div>
+    <ProfileCard />
   );
 }
 
