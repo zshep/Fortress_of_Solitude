727ce31b709d4e1ca0ae61c56b91a5e53829db9f
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-12-09 12:43:16 -0800

    create job working, need to uncomment mutation on CreatePost once server ready

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,58 +1,127 @@
-import React from 'react'
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { CREATE_JOB } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function CreatePost() {
+  const [createJob, { error }] = useMutation(CREATE_JOB);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [summary, setSummary] = useState("");
+  const changeState = (event) => {
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    switch (inputType) {
+      case "title":
+        setTitle(inputValue);
+        break;
+      case "category":
+        setCategory(inputValue);
+        break;
+      default:
+        setSummary(inputValue);
+        break;
+    }
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      throw new Error(
+        "You need to be logged in to save a job. How did you get here?"
+      );
+    }
+    const jobData = {
+      category,
+      title,
+      summary,
+    };
+    try {
+      console.log(jobData)
+      // const { data } = await createJob({
+      //   variables: { jobData },
+      // });
+      // if (!data) {
+      //   throw new Error("No data returned");
+      // }
+    } catch (error) {
+      throw new Error("Failed to save job.");
+    }
+  };
+
   return (
-    <div class="container box p-6
-                has-background-light">
-      <h2 class="subtitle has-text-centered">
+    <div
+      className="container box p-6
+                has-background-light"
+    >
+      <h2 className="subtitle has-text-centered">
         Volunteer Opportunity #xxxxxx
       </h2>
       <form action="">
-        <div class="field">
-          <label class="label">Title of Your Post</label>
-          <div class="control">
-            <input class="input" type="text"
-                   placeholder="'Litter at the park' etc.." />
+        <div className="field">
+          <label className="label">Title of Your Post</label>
+          <div className="control">
+            <input
+              className="input"
+              name="title"
+              type="text"
+              placeholder="'Litter at the park' etc.."
+              onChange={changeState}
+            />
           </div>
         </div>
-  
-        <div class="field">
-          <label class="label">What Category of work is this?</label>
-          <div class="control has-icons-left
-                      has-icons-right">
+
+        <div className="field">
+          <label className="label">What Category of work is this?</label>
+          <div
+            className="control has-icons-left
+                      has-icons-right"
+          >
             <input
-              class="input"
+              className="input"
+              name="category"
               type="text"
               placeholder=" Cleaning, Repair, Construction etc..."
+              onChange={changeState}
             />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope"></i>
             </span>
           </div>
         </div>
-        <div class="field">
-          <label class="label">
-            Volunteer Opportunity Description
-          </label>
-          <div class="control">
+        <div className="field">
+          <label className="label">Volunteer Opportunity Description</label>
+          <div className="control">
             <textarea
-              class="textarea"
+              className="textarea"
               placeholder="How can someone help?"
+              onChange={changeState}
             ></textarea>
           </div>
         </div>
-  
-  
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button" onMouseOver={(e) => {e.target.style.transform = 'scale(1.25)';}} onMouseOut={(e) => {e.target.style.transform = 'scale(1)';}}  style={{ fontFamily: 'Permanent Marker', background: '#A6CE90'  }}>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              className="button"
+              onClick={handleSubmit}
+              onMouseOver={(e) => {
+                e.target.style.transform = "scale(1.25)";
+              }}
+              onMouseOut={(e) => {
+                e.target.style.transform = "scale(1)";
+              }}
+              style={{ fontFamily: "Permanent Marker", background: "#A6CE90" }}
+            >
               Create Post
             </button>
           </div>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default CreatePost
+export default CreatePost;
