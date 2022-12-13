10ff02bc97334e9b23e41c35f834255647c1d386
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-12-13 15:16:37 -0800

    adding some starter fucntionality to accepted chore card

--- client/src/components/Card/AcceptedChoreCard.js
+++ client/src/components/Card/AcceptedChoreCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Container from "../RandomComponents/Container.js";
+import { Link } from "react-router-dom";
 import Columnsss from "../Column/Columnsss";
 import SmallGreenSticky from "../StickyNotes/SmallGreenSticky";
 import SmallStickyNote from "../StickyNotes/SmallStickyNote";
 
 function AcceptedChoreCard({ data }) {
+
+ 
+    console.log(data)
+  
   return (
     <>
       <Container attr={"has-text-centered"}>
@@ -18,19 +23,23 @@ function AcceptedChoreCard({ data }) {
           {data.acceptedChores.map((el, i) => {
             return (i + 1) % 2 === 0 ? (
               <div className="column is-one-third" key={el.id} postid={el._id}>
+                <Link to={`/post/${el._id}`}>
                 <SmallGreenSticky
                   title={el.title}
                   summary={el.body}
                   
                 />
+                </Link>
               </div>
             ) : (
               <div className="column is-one-third" key={el.id} postid={el._id}>
+                <Link to={`/post/${el._id}`}>
                 <SmallStickyNote
                   title={el.title}
                   summary={el.body}
                   
                 />
+                </Link>
               </div>
             );
           })}
