9cd04bf4a0b7a4241be2f153bdcaa6d686671e1a
Patrick <sobczyk1290@gmail.com>
2022-12-16 11:37:16 -0800

    trying without the *

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // is this stuff used to set up for Heroku?
   app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
