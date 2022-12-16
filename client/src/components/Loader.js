87baad1ab3b20a351695265dcc537234cd733a7e
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-12-15 20:33:07 -0800

    stylized div

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,8 +1,31 @@
 import React from "react"
 
-function Loader{
+const styledLoader = styled.div`.spinner {
+    margin:20px auto 20px auto;
+    position:relative;
+    -webkit-animation: spin 1s infinite ease-in;
+    animation: spin 1s infinite linear;
+    height: 65px;
+    width: 40px;
+    background:  url(http://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Hammer_-_Noun_project_1306.svg/1088px-Hammer_-_Noun_project_1306.svg.png); 
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  
+  @-webkit-keyframes spin {
+      0% { -webkit-transform: rotate(0deg); }
+      50% { -webkit-transform: rotate(55deg); }
+  }
+  @keyframes spin {
+      from {transform:rotate(0deg);}
+      to {transform:rotate(360deg);}
+  }`
+function Loader(){
 
-return <div class='spinner'></div>}
+return (
+<div class='spinner'></div>
+
+)}
 
 
 module.exports = Loader 
