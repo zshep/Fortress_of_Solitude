d0683edbf0f20b03dee53f1ddd95d4d8be4bd5ab
tonytran97 <tonytran_97@live.com>
2022-12-18 21:26:30 -0800

    + login CSS changes to text block

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,33 +14,33 @@ const Login = () => {
     window.location.assign("/profile")
   ) : (
     <>
-    <LoginBanner />
+      <LoginBanner />
       <Section>
         <Container>
           <Columnsss attr={"is-centered is-vcentered"}>
             <Column attr={"is-half"}>
               <LoginCard />
-              
-              
+
+
             </Column>
-            
+
           </Columnsss>
-          
+
         </Container>
         <Container attr={"container has-text-centered"}>
 
-<div className="pt-5 loginBackground">
-          <p className="has-text-white pt-5 "> This site is a by-invite site only so that you can keep your community tight. </p>
-          <p className="has-text-white">The administrator of your community should have provided you with your login credentials. Contact them for your credentials.</p>
-          <p className="has-text-white"> If you would like to create a GoMode community, contact one of the devs listed at the footer of this page</p>
+          <div className="loginBackground">
+            <p className="has-text-white"> This site is a by-invite site only so that you can keep your community tight. </p>
+            <p className="has-text-white">The administrator of your community should have provided you with your login credentials. Contact them for your credentials.</p>
+            <p className="has-text-white"> If you would like to create a GoMode community, contact one of the devs listed at the footer of this page</p>
           </div>
 
         </Container>
-        
+
       </Section>
-  
-      
-      
+
+
+
     </>
   );
 };
